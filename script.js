b4036423e558bcce5fe0816f08c8a53f1685daee
HAJI113 <91335947+HAJI113@users.noreply.github.com>
2021-12-09 16:50:46 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function openNav(){
+          document.getElementById('links').style.display ="block";
+}
+function closeNav(){
+          document.getElementById('links').style.display ="none";
+}
+
+
+function openPop(){
+          document.getElementById('pop').style.display ="flex";
+}
+function popNav(){
+          document.getElementById('pop').style.display ="none";
+}
